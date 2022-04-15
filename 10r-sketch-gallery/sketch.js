16ac0b0dd79e9635fb9e83ea5e5c8202fc5a822c
moqn <moonjunghyun@gmail.com>
2022-04-15 22:07:32 +0800

    Initial commit

---
+++ 10r-sketch-gallery/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  let cvs = createCanvas(800, 500);
+  cvs.parent("my-container");
+  background(220);
+}
+
+function draw() {
+  fill(random(255), random(255), random(255));
+  circle(random(width), random(height), random(10, 50));
+}
